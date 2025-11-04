86f15cc41dfc8525d6626658c0e5a99ab9b099c6
Jake Chalmers <jakechalmers86@gmail.com>
2025-11-04 20:43:24 +0800

    Updated app.listen to call connectDB function

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv"; 
+import { connectDB } from "./config/db.js";    
 
 dotenv.config();
 
@@ -10,7 +11,12 @@ app.get("/prducts", (req, res) => {});
 console.log(process.env.MONGO_URI);
 
 app.listen(5000, () => {
-    console.log("server started at http://localhost:5000");
+    connectDB();
+    console.log("server started at http://localhost:5000 ");
 });
+
+
+
+
 
 // MongoDB PassWord: tLwtTwkt9xVFPWoA
